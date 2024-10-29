b6f31b5d083af8414eb827ad6d9d495fae7554ec
Isla <git@isla.dev>
2024-10-30 00:16:42 +0100

    Fixed filtering

--- src/Aspire.Dashboard/Components/Controls/OpenApiResponse.razor.cs
+++ src/Aspire.Dashboard/Components/Controls/OpenApiResponse.razor.cs
@@ -14,19 +14,27 @@ public sealed partial class OpenApiResponse : ComponentBase
     private string _filter = string.Empty;
     private IQueryable<OpenApiResponseHeader> _headers = null!;
 
+    private IQueryable<OpenApiResponseHeader> FilteredHeaders => _headers.Where(header => header.MatchesFilter(_filter));
+
     protected override async Task OnInitializedAsync()
     {
         _body = await Response.Content.ReadAsStringAsync();
         _headers = Response.Content.Headers.AsQueryable().Select(x => new OpenApiResponseHeader
         {
             Name = x.Key,
-            Value = x.Value.FirstOrDefault()
+            Value = x.Value.FirstOrDefault() ?? string.Empty
         });
     }
 
     public class OpenApiResponseHeader : IPropertyGridItem
     {
         public required string Name { get; init; }
-        public required string? Value { get; init; }
+        public required string Value { get; init; }
+
+        public bool MatchesFilter(string filter)
+        {
+            return Name.Contains(filter, StringComparison.CurrentCultureIgnoreCase) ||
+                   Value.Contains(filter, StringComparison.CurrentCultureIgnoreCase);
+        }
     }
 }
